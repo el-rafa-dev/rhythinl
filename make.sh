9604c32e3a7599647058ad559f557329cf01d324
el-rafa-dev <rafaelsousa12365@gmail.com>
2025-06-27 10:36:58 -0300

    added a make.sh to clean older builds and starts new builds of  rhythin in 'build' directoy

--- make.sh
+++ make.sh
@@ -4,9 +4,7 @@
 
 function cd_and_start {
     curr_dir=$(pwd)
-
     cd "build" || { echo "Error: Could not change to 'build' directory. Aborting."; exit 1; }
-
     if [[ "$(basename "$(pwd)")" == "build" ]]; then
         echo -e "Entered 'build' directory.\n"
 
